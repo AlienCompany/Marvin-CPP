85ed0d0aba36a0eed5cee4269d69f38355d7d44f
Unknown <hugues.vogel@hotmail.fr>
2018-07-20 20:43:32 +0200

    wip

--- Com.cpp
+++ Com.cpp
@@ -4,12 +4,32 @@
 
 #include "Com.h"
 
+bool debugReceive(Commande commande);
+
 void checkReceive(){
     while(Serial.available()){
         Commande commande;
         Serial.readBytes((byte *) &commande, sizeof(Commande));
-        onReceive(commande);
+        if(!debugReceive(commande)) onReceive(commande);
     }
+}
+
+bool debugReceive(Commande commande) {
+
+    switch (commande.id){
+        COMMANDE_DEBUG_SCOP = 0xD0,
+        COMMANDE_DEBUG_DEL_SCOP = 0xD1,
+        COMMANDE_DEBUG_ADD_VAR = 0xD2,
+        COMMANDE_DEBUG_DEL_VAR = 0xD3,
+        COMMANDE_DEBUG_ONCHANGE_VAR = 0xD4,
+        COMMANDE_DEBUG_LOG = 0xD5,
+
+        COMMANDE_DEBUG_SET_VAR = 0xE5,
+        COMMANDE_DEBUG_PAUSE = 0xE1,
+        COMMANDE_DEBUG_PLAY = 0xE0,
+        COMMANDE_DEBUG_STOPTIME = 0xE0,
+    }
+    return false;
 }
 
 void sendCommande(CommandesId id, uint32_t data){
@@ -37,4 +57,105 @@ Commande_16 to_16(Commande &commande){
 }
 Commande_8 to_8(Commande &commande){
     return (Commande_8&) commande;
+}
+
+void d_sendDebug(CommandesId id) {
+    sendCommande(id,0);
+}
+
+void d_sendDebug(CommandesId id, String s0) {
+    d_sendDebug(id,s0,"","","");
+}
+
+void d_sendDebug(CommandesId id, String s0, String s1) {
+    d_sendDebug(id,s0,s1,"","");
+}
+
+void d_sendDebug(CommandesId id, String s0, String s1, String s2) {
+    d_sendDebug(id,s0,s1,s2,"");
+}
+
+void d_sendDebug(CommandesId id, String s0, String s1, String s2, String s3) {
+    sendCommande(id,s0.length(), s1.length(), s2.length(), s3.length());
+    Serial.print(String() += s0 += s1 += s2 += s3);
+}
+
+void d_newScop(String name) {
+    d_scope* newScope = new d_scope(name);
+    d_scopes.pushBack(newScope);
+    d_sendDebug(COMMANDE_DEBUG_SCOP,name,String(newScope->id));
+}
+
+int32_t d_getLastScope(String name, d_scope *&res) {
+    for(uint16_t i = d_scopes.getLength()-1;i >= 0; i--){
+        res = d_scopes.get(i);
+        if(name.equals(res->name)) return i;
+    }
+    return -1;
+}
+
+void log(String type, String fileName, String functionName, String msg) {
+    d_sendDebug(COMMANDE_DEBUG_LOG,type,fileName,functionName,msg);
+}
+
+void d_delScop(String name) {
+    d_scope *scope;
+    int32_t index = d_getLastScope(name, scope);
+    if(index == -1) {
+        log("ERROR", "com.cpp", "d_delScop",String("Scope '") += name += "' n'exist pas");
+
+        return;
+    }
+    d_sendDebug(COMMANDE_DEBUG_DEL_SCOP, name, String(scope->id));
+    delete scope;
+    d_scopes.set(index, d_scopes.removeLast());
+}
+
+void d_addVar(String scop, String name, d_types type, void *valuePtr, String value) {
+    d_sendDebug(COMMANDE_DEBUG_ADD_VAR, scop, name, String() += type, value);
+
+}
+
+void d_addVar(String scop, String name, uint64_t &value) {
+    d_addVar(scop,name,UINT64_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, uint32_t &value) {
+    d_addVar(scop,name,UINT32_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, uint16_t &value) {
+    d_addVar(scop,name,UINT16_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, uint8_t &value) {
+    d_addVar(scop,name,UINT8_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, int64_t &value) {
+    d_addVar(scop,name,INT64_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, int32_t &value) {
+    d_addVar(scop,name,INT32_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, int16_t &value) {
+    d_addVar(scop,name,INT16_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, int8_t &value) {
+    d_addVar(scop,name,INT8_T, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, float &value) {
+    d_addVar(scop,name,FLOAT, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, double &value) {
+    d_addVar(scop,name,DOUBLE, &value, String() += value);
+}
+
+void d_addVar(String scop, String name, char &value) {
+    d_addVar(scop,name,CHAR, &value, String() += value);
 }
--- Com.h
+++ Com.h
@@ -2,6 +2,7 @@
 // Created by hugues on 11/06/2018.
 //
 #include <Arduino.h>
+#include "ArrayAutoSize.h"
 
 #ifndef TEST2_COM_H
 #define TEST2_COM_H
@@ -32,6 +33,31 @@ enum CommandesId : uint8_t {
 
     TEST_COMMANDE_ALLUMER_LED = 0xF1, // data = time ms
     TEST_COMMANDE_RESAVE = 0xF2,
+
+    COMMANDE_DEBUG_SCOP = 0xD0,
+    COMMANDE_DEBUG_DEL_SCOP = 0xD1,
+    COMMANDE_DEBUG_ADD_VAR = 0xD2,
+    COMMANDE_DEBUG_DEL_VAR = 0xD3,
+    COMMANDE_DEBUG_ONCHANGE_VAR = 0xD4,
+    COMMANDE_DEBUG_LOG = 0xD5,
+
+    COMMANDE_DEBUG_SET_VAR = 0xE5,
+    COMMANDE_DEBUG_PAUSE = 0xE1,
+    COMMANDE_DEBUG_PLAY = 0xE0,
+    COMMANDE_DEBUG_STOPTIME = 0xE0,
+};
+enum d_types: uint8_t{
+    UINT64_T = 0x01,
+    UINT32_T = 0x02,
+    UINT16_T = 0x03,
+    UINT8_T = 0x04,
+    INT64_T = 0x05,
+    INT32_T = 0x06,
+    INT16_T = 0x07,
+    INT8_T = 0x08,
+    CHAR = 0x09,
+    DOUBLE = 0x0A,
+    FLOAT = 0x0B,
 };
 
 struct Commande {
@@ -52,6 +78,30 @@ struct Commande_8 {
     uint8_t data2;
     uint8_t data3;
 };
+struct d_var{
+    String name;
+    d_types types;
+    void* varPtr;
+};
+
+uint16_t nbScope = 0;
+
+struct d_scope{
+    d_scope(String name){
+        d_scope::name = name;
+        id = nbScope++;
+        vars = ArrayAutoSize<d_var>();
+    }
+    d_scope(){
+        d_scope("");
+    }
+    uint16_t id;
+    String name;
+    ArrayAutoSize<d_var> vars;
+};
+
+d_scope d_globalScope = d_scope();
+ArrayAutoSize<d_scope*> d_scopes = ArrayAutoSize<d_scope*>();
 
 void onReceive(Commande &commande);
 
@@ -69,5 +119,29 @@ Commande_16 to_16(Commande &commande);
 
 Commande_8 to_8(Commande &commande);
 
+void d_sendDebug(CommandesId id);
+void d_sendDebug(CommandesId id, String s0);
+void d_sendDebug(CommandesId id, String s0, String s1);
+void d_sendDebug(CommandesId id, String s0, String s1, String s2);
+void d_sendDebug(CommandesId id, String s0, String s1, String s2, String s3);
+
+void d_newScop(String name);
+int32_t d_getLastScope(String name, d_scope* &res);
+
+void log(String type, String fileName, String functionName, String msg);
+
+void d_delScop(String name);
+void d_addVar(String scop, String name, d_types type, void* valuePtr, String value);
+void d_addVar(String scop, String name, uint64_t &value);
+void d_addVar(String scop, String name, uint32_t &value);
+void d_addVar(String scop, String name, uint16_t &value);
+void d_addVar(String scop, String name, uint8_t &value);
+void d_addVar(String scop, String name, int64_t &value);
+void d_addVar(String scop, String name, int32_t &value);
+void d_addVar(String scop, String name, int16_t &value);
+void d_addVar(String scop, String name, int8_t &value);
+void d_addVar(String scop, String name, float &value);
+void d_addVar(String scop, String name, double &value);
+void d_addVar(String scop, String name, char &value);
 
 #endif //TEST2_COM_H
